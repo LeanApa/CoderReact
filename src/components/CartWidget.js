dafbac59427de09414c9b71c0a702873e88ed949
Leandro Apablaza <lea.apagro@gmail.com>
2022-11-26 21:59:58 -0300

    Primer desafio react

---
+++ src/components/CartWidget.js
@@ -0,0 +1,10 @@
+import {BsFillCartCheckFill} from "react-icons/bs";
+
+export const CartWidget = () => {
+
+    return (
+        <div>
+            <BsFillCartCheckFill size={50}/>
+        </div>
+    )
+}

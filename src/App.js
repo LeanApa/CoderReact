6838c22e74bb58d72dd16194b397633ba54959ca
LeanApa <lea.apagro@gmail.com>
2022-12-10 10:55:02 -0300

    Cambios de estilos

--- src/App.js
+++ src/App.js
@@ -11,15 +11,23 @@ const App = () => {
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<ItemListContainer greeting={"Hola Leandro!"} />}/>
-        <Route path="/category/:id" element={<ItemListContainer greeting={"Hola Leandro!"} />}/>
-        <Route path="/category/" element={<ItemListContainer greeting={"Hola Leandro!"} />}/>
+        <Route
+          path="/"
+          element={<ItemListContainer greeting={"Hola Leandro!"} />}
+        />
+        <Route
+          path="/category/:id"
+          element={<ItemListContainer greeting={"Hola Leandro!"} />}
+        />
+        <Route
+          path="/category/"
+          element={<ItemListContainer greeting={"Hola Leandro!"} />}
+        />
 
-        <Route path="/itemDetail/:id" element={<ItemDetailContainer/>}/>
-        <Route path="/cart" element={<h2>Esto es el carrito</h2>}/>
-        <Route path="*" element={<h1>La url no existe</h1>}/>
+        <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
+        <Route path="/cart" element={<h2>Esto es el carrito</h2>} />
+        <Route path="*" element={<h1>La url no existe</h1>} />
       </Routes>
-      
     </BrowserRouter>
   );
 };
